b04d9cc5983794886aa2a47b82468929f1b79a3b
Ranffi <ranffiramirez@gmail.com>
2022-04-27 14:22:55 +0000

    basic flask setup

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Astronote'
+# db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
